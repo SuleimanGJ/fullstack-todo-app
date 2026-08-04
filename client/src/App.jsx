bd12ca2b2b2c91f914db5757135490b3f03335ab
SuleimanByte <suleimanosman1023@gmail.com>
2026-08-04 10:47:24 +0300

    added basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 
 function App() {
   return (
-    <>
-      App
-    </>
+    <div>
+      <Header />
+      <Outlet />
+    </div>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import SigninPage from "./pages/SigninPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "register",
+        element: <SignupPage />,
+      },
+      {
+        path: "login",
+        element: <SigninPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
