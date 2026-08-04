de49234eab99a0adba01ef4b6844dbd87f5d2b50
SuleimanByte <suleimanosman1023@gmail.com>
2026-08-04 17:34:04 +0300

    added signup logic

--- client/src/components/auth/SignupForm.jsx
+++ client/src/components/auth/SignupForm.jsx
@@ -1,15 +1,46 @@
-
+import { useNavigate } from "react-router-dom"
 
 function SignupForm() {
+  const navigate = useNavigate()
+  const handleLogin =() => {
+    navigate('/login')
+  }
   return (
-    <div>
-      <h1>Sign Up</h1>
-      {/* <form>
-        <label htmlFor=""></label>
-
-      </form> */}
+    <div className="p-6 max-w-xl text-center bg-gray-400 rounded-2xl shadow-amber-300">
+      <h1 className="text-2xl">Signup</h1>
+      <form className=" py-6 flex flex-col text-left">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          className="my-3 rounded border-2 border-solid py-2"
+          placeholder="Enter your username"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="my-3 rounded border-2 border-solid py-2"
+          placeholder="Enter your email"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className="my-3 rounded border-2 border-solid py-2"
+          placeholder="Enter your password"
+        />
+        <button className="my-3 bg-green-700 py-2 px-1 rounded">Signup</button>
+      </form>
+      {/*  Login is a link */}
+      <p>
+        Already have an account? <span onClick={handleLogin}>Login</span>
+      </p>
     </div>
-  )
+  );
 }
 
 export default SignupForm
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -5,7 +5,7 @@ import SignupForm from "../components/auth/SignupForm";
 
 const SignupPage = () => {
   return (
-    <div className="flex ">
+    <div className="flex justify-center items-center my-6 ">
       <SignupForm />
     </div>
   );
