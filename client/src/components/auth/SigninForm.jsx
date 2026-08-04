a77adabe0d3c7235edd816afcda1376ee8f80a4b
SuleimanByte <suleimanosman1023@gmail.com>
2026-08-04 10:48:51 +0300

    added basic skeleton signin form

--- client/src/components/auth/SigninForm.jsx
+++ client/src/components/auth/SigninForm.jsx
@@ -2,7 +2,28 @@
 
 function SigninForm() {
   return (
-    <div>SigninForm</div>
+    <div className=" p-6 max-w-full text-center bg-gray-400 shadow-amber-300">
+      <h1>Signin</h1>
+      <form className=" py-6 flex flex-col text-left">
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+        <button>Signin</button>
+      </form>
+      {/*  register is a link */}
+      <p>Don't have an account? Register</p>
+    </div>
   );
 }
 
