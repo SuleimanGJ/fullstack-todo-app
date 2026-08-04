03d78f4336e99b90fd92ba26680dff872fa780e7
SuleimanByte <suleimanosman1023@gmail.com>
2026-08-04 17:33:33 +0300

    added register button logic

--- client/src/components/auth/SigninForm.jsx
+++ client/src/components/auth/SigninForm.jsx
@@ -1,14 +1,17 @@
-
+import { useNavigate } from "react-router-dom";
 
 function SigninForm() {
-
+    const navigate = useNavigate();
+    const handleRegister = () => {
+      navigate("/register");
+    };
   return (
     <div className="p-6 max-w-xl text-center bg-gray-400 rounded-2xl shadow-amber-300">
       <h1 className="text-2xl">Signin</h1>
       <form className=" py-6 flex flex-col text-left">
         <label htmlFor="email">Email</label>
         <input
-          type="text"
+          type="email"
           id="email"
           name="email"
           className="my-3 rounded border-2 border-solid py-2"
@@ -25,9 +28,9 @@ function SigninForm() {
         <button className="my-3 bg-green-700 py-2 px-1 rounded">Signin</button>
       </form>
       {/*  register is a link */}
-      <p>Don't have an account? Register</p>
+      <p>Don't have an account? <span onClick={handleRegister}>Register</span></p>
     </div>
   );
 }
 
-export default SigninForm
+export default SigninForm;
